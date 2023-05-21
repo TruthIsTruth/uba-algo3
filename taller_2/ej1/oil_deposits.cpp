ae60235aa06c1dc9f24dbef88bf99ef92ddc000f
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-21 18:05:24 -0300

    Taller 2 - Ej1 Completado

--- taller_2/ej1/oil_deposits.cpp
+++ taller_2/ej1/oil_deposits.cpp
@@ -6,17 +6,12 @@
 
 using namespace std;
 
-enum {BLANCO, GRIS, NEGRO};
 int n, m;
 vector<vector<bool>> M; //Mapa de Petroleo
-vector<vector<int>> color; //Matriz de visitados
+vector<vector<bool>> visitado; //Matriz de visitados
 
-int dfs(pair<int,int> u){
-    color[u.first][u.second] = GRIS;
-    if (!M[u.first][u.second]){
-        color[u.first][u.second] = NEGRO;
-        return 0;
-    }
+void dfs(pair<int,int> u){
+    visitado[u.first][u.second] = true;
 
     for (int y = -1; y <= 1; y++) {
         for (int x = -1; x <= 1; x++) {
@@ -24,22 +19,20 @@ int dfs(pair<int,int> u){
                 continue;
             if (u.first + y < 0 || u.first + y >= n || u.second + x < 0 || u.second + x >= m)
                 continue;
-            pair<int,int> coord;
+            pair<int,int> adj(u.first + y, u.second + x);
+            if (M[adj.first][adj.second] && !visitado[adj.first][adj.second])
+                dfs(adj);
         }
     }
-    
-
-    dfs(make_pair(u.first-1, u.second-1));  //Top-Left
-    dfs(make_pair(u.first-1, u.second));    //Top
-    dfs(make_pair(u.first-1, u.second+1));  //Top-Right
-    dfs(make_pair(u.first, u.second-1));    //Left
-    dfs(make_pair(u.first, u.second+1));    //Right
-    dfs(make_pair(u.first+1, u.second-1));  //Bottom-Left
-    dfs(make_pair(u.first+1, u.second));    //Bottom
-    dfs(make_pair(u.first+1, u.second+1));  //Bottom-Right
-
-    color[u] = NEGRO;
-    
+    /* Idea */
+    // dfs(make_pair(u.first-1, u.second-1));  //Top-Left
+    // dfs(make_pair(u.first-1, u.second));    //Top
+    // dfs(make_pair(u.first-1, u.second+1));  //Top-Right
+    // dfs(make_pair(u.first, u.second-1));    //Left
+    // dfs(make_pair(u.first, u.second+1));    //Right
+    // dfs(make_pair(u.first+1, u.second-1));  //Bottom-Left
+    // dfs(make_pair(u.first+1, u.second));    //Bottom
+    // dfs(make_pair(u.first+1, u.second+1));  //Bottom-Right
 }
 
 int main() {
@@ -48,9 +41,7 @@ int main() {
         if (n+m==0) break;
 
         M.assign(n, vector<bool>(m, false));
-        C.assign(n, vector<int>(m, BLANCO));
-        
-        //map<pair<int, int>,int> coord_to_index;
+        visitado.assign(n, vector<bool>(m, false));
 
         int pozos = 0;
         for (int i=0; i<n; i++) {
@@ -58,7 +49,6 @@ int main() {
                 char pos; cin >> pos;
                 if (pos == '@'){
                     M[i][j] = true;
-                    //coord_to_index[make_pair(i,j)] = pozos;
                     pozos++;
                 }
             }
@@ -67,12 +57,13 @@ int main() {
         int cc = 0;
         for (int i=0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                cc += dfs(make_pair(i,j));
+                if (M[i][j] && !visitado[i][j]){
+                    cc++;
+                    dfs(make_pair(i,j));
+                }
             }
         }
-        
-
-
+        cout << cc << endl;
     }
 
     return 0;
